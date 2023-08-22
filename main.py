dac6b7e58b62475bbf65bb4ba8e748159b24e025
Brandon0902 <115058579+Brandon0902@users.noreply.github.com>
2023-08-22 13:06:41 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,89 +1,203 @@
-from models import Equipo, Jugador,Torneo,DirectorTecnico,TablaPosiciones,EquipoTorneo,Partido,Resultado
+from models import Equipo, Jugador, Torneo, DirectorTecnico, Partido, TablaPosiciones, Resultado, EquipoTorneo
 
-# Crear los equipos
-atlas = Equipo("logotipo1.png", [], "dir_tecnico1", "Atlas")
-america = Equipo("logotipo2.png", [], "dir_tecnico2", "America")
-chivas = Equipo("logotipo3.png", [], "dir_tecnico3", "Chivas")
+def mostrar_menu():
+     print("Menu:")
+     print("1. Agregar equipo")
+     print("2. Agregar jugadores")
+     print("3. Agregar equipos al torneo")
+     print("4. Agregar partidos al torneo")
+     print("5. Ingresar resultados de los partidos")
+     print("6. Mostrar Jugadores")
+     print("7. Mostrar partidos")
+     print("8. Mostrar tabla de Posiciones")
+     print("9. Salir")
 
-# Agregar jugadores a los equipos
-print("Jugadores")
-print("-------------------")
-atlas.agregar_jugador(Jugador("Julian Quiñones", 33, "Colombiano", "Delantero", "Titular"))
-atlas.agregar_jugador(Jugador("Julio Furch", 9, "Argentino", "Delantero", "Titular"))
-atlas.agregar_jugador(Jugador("Oziel Herrera", 7, "Mexicano/Cubano", "Extremo", "Titular"))
+def agregar_equipo():
+    logotipo = input("Ingrese el logotipo del equipo: ")
+    nombre = input("Ingrese el nombre del equipo: ")
+   
+    nombre_dt = input("Ingrese el nombre del director técnico: ")
+    nacionalidad_dt = input("Ingrese la nacionalidad del director técnico: ")
+    porcentaje_dt = None
+    while porcentaje_dt is None:
+        try:
+            porcentaje_dt = int(input("Ingrese el porcentaje del director técnico: "))
+        except ValueError:
+            print("Error: Porcentaje inválido. Ingrese un valor numérico entero.")
 
-america.agregar_jugador(Jugador("Jonathan Rodriguez", 10, "Uruguayo", "Extremo", "Titular"))
-america.agregar_jugador(Jugador("Henry Martin", 21, "Mexicano", "Delantero", "Titular"))
-america.agregar_jugador(Jugador("Alejandro Zendejas", 17, "Estadounidense", "Extremo", "Titular"))
-
-chivas.agregar_jugador(Jugador("Pavel Perez", 6, "Mexicano", "Delantero", "Suplente"))
-chivas.agregar_jugador(Jugador("Alexis Vega", 10, "Mexicano", "Extremo", "Titular"))
-chivas.agregar_jugador(Jugador("Roberto Alvarado", 26, "Mexicano", "Extremo", "Titular"))
-
-for equipo in [atlas, america, chivas]:
-    print(f"Equipo: {equipo.nombre}")
-    for jugador in equipo.jugadores:
-        print(f"Jugador: {jugador.nombre}, número: {jugador.numero}, nacionalidad: {jugador.nacionalidad}, posición: {jugador.posicion}, rol: {jugador.rol}") 
-print("-------------------")
-# Crear el torneo
-torneo = Torneo([atlas, america, chivas], [], [])
-
-# Agregar un partido al torneo
-torneo.agregar_partido(atlas, america, "Estadio Jalisco", "2023-05-09")
-torneo.agregar_partido(chivas, atlas, "Estadio Akron", "2023-06-01")
-torneo.agregar_partido(america, chivas, "Estadio Azteca", "2023-07-13")
-
-#Crear directores tecnicos
-dir_tecnico_atlas = DirectorTecnico("Benjamin Mora", "Mexicano", 40.0)
+    dt = DirectorTecnico(nombre_dt, nacionalidad_dt, porcentaje_dt)
+    
+    jugadores = []
+    return Equipo(logotipo, jugadores, dt, nombre)
 
 
-dir_tecnico_america = DirectorTecnico("Fernando ortiz", "Mexicano", 80.0)
+def agregar_jugadores(equipos):
+    mostrar_equipos(equipos)
+    num_equipo = int(input("Seleccione el número del equipo al que desea agregar jugadores: ")) - 1
+    
+    if 0 <= num_equipo < len(equipos):
+        equipo_seleccionado = equipos[num_equipo]
+        num_jugadores = int(input("Ingrese la cantidad de jugadores a agregar: "))
+        for i in range(num_jugadores):
+            print(f"\nJugador {i+1}:")
+            nombre_jugador = input("Ingrese el nombre del jugador: ")
+            numero = input("Ingrese el número del jugador: ")
+            nacionalidad = input("Ingrese la nacionalidad del jugador: ")
+            posicion = input("Ingrese la posición del jugador: ")
+            rol = input("Ingrese el rol del jugador: ")
+
+            jugador = Jugador(nombre_jugador, numero, nacionalidad, posicion, rol)
+            equipo_seleccionado.jugadores.append(jugador)
+        print("Jugadores agregados con éxito.")
+    else:
+        print("Número de equipo inválido.")
+
+def mostrar_equipos(equipos):
+    print("Equipos disponibles:")
+    for i, equipo in enumerate(equipos, start=1):
+        print(f"{i}. {equipo.nombre}")
+
+def crear_torneo():
+    nombre_torneo = input("Ingrese el nombre del torneo: ")
+    torneo = Torneo(nombre_torneo)
+    torneo.tabla_posiciones = TablaPosiciones(torneo)  # Crear una instancia de TablaPosiciones
+    return torneo
 
 
-dir_tecnico_chivas = DirectorTecnico("Veljko Paunović", "Español", 60.0)
+def agregar_equipos_al_torneo(torneo, equipos):
+    mostrar_equipos(equipos)
+    equipos_seleccionados = []
+
+    while True:
+        num_equipo = int(input("Seleccione el número del equipo que desea agregar al torneo (0 para terminar): "))
+        
+        if num_equipo == 0:
+            break
+
+        if 1 <= num_equipo <= len(equipos):
+            equipo_seleccionado = equipos[num_equipo - 1]
+            equipos_seleccionados.append(equipo_seleccionado)
+            print(f"Equipo '{equipo_seleccionado.nombre}' agregado al torneo.")
+        else:
+            print("Número de equipo inválido.")
+
+    for equipo in equipos_seleccionados:
+        torneo.agregar_equipo(equipo)
 
 
-#Instancias de EquipoTorneo
-atlas_torneo = EquipoTorneo(atlas,10, 12, 3, 3, 1)
-america_torneo= EquipoTorneo(america,8, 9, 2, 3, 2)
-chivas_torneo = EquipoTorneo(chivas, 7, 1, 4, 1,2)
+def agregar_partido_al_torneo(torneo):
+    equipo1_nombre = input("Ingrese el nombre del primer equipo: ")
+    equipo2_nombre = input("Ingrese el nombre del segundo equipo: ")
+    estadio = input("Ingrese el nombre del estadio: ")
+    fecha = input("Ingrese la fecha del partido: ")
 
-# Crea la Tabla
-tabla = TablaPosiciones("Torneo Apertura", [atlas_torneo, america_torneo, chivas_torneo])
+    equipo1 = None
+    equipo2 = None
 
-# Mostrar la tabla de posiciones
-print("Tabla de Posiciones")
-print("-------------------")
-print("Equipo\tGoles\tPuntos\tPG\tPE\tPP")
-for i, equipo_toreno in enumerate(tabla.equipos_torneo, start=1):
-    print(f"{i}.{equipo_toreno.equipo.nombre}\t{equipo_toreno.goles}\t{equipo_toreno.puntos}\t{equipo_toreno.partidos_ganados}\t{equipo_toreno.partidos_empatados}\t{equipo_toreno.partidos_perdidos}")
-print("-------------------")
+    for equipo in torneo.equipos:
+        if equipo.nombre == equipo1_nombre:
+            equipo1 = equipo
+        elif equipo.nombre == equipo2_nombre:
+            equipo2 = equipo
 
-#Crear partidos
-print("Partidos")
-print("-------------------")
+    if equipo1 is None or equipo2 is None:
+        print("Error: Los equipos ingresados no están registrados.")
+        return
 
-partido1 = Partido(atlas, america, "Estadio Jalisco", "2023-05-09")
-partido2 = Partido(chivas, atlas, "Estadio Akron", "2023-06-01")
-partido3 = Partido(america, chivas, "Estadio Azteca", "2023-07-13")
+    partido = Partido(equipo1, equipo2, estadio, fecha)
+    torneo.partidos.append(partido)
+    print("Partido agregado con éxito.")
+
+def ingresar_resultados(torneo):
+    fecha_partido = input("Ingrese la fecha del partido (dd/mm/yyyy): ")
+    equipo1_nombre = input("Ingrese el nombre del primer equipo: ")
+    equipo2_nombre = input("Ingrese el nombre del segundo equipo: ")
+    goles_equipo1 = int(input("Ingrese la cantidad de goles del primer equipo: "))
+    goles_equipo2 = int(input("Ingrese la cantidad de goles del segundo equipo: "))
+
+    partido_encontrado = None
+    for partido in torneo.partidos:
+        if partido.fecha == fecha_partido and partido.equipo1.nombre == equipo1_nombre and partido.equipo2.nombre == equipo2_nombre:
+            partido_encontrado = partido
+            break
+
+    if partido_encontrado is not None:
+        resultado = Resultado(partido_encontrado, goles_equipo1, goles_equipo2)
+        torneo.registrar_resultado(resultado)
+        print("Resultado registrado con éxito.")
+    else:
+        print("Error: No se encontró el partido.")
 
 
-partido1.mostrar_partido()
-partido2.mostrar_partido()
-partido3.mostrar_partido()
+def mostrar_jugadores(equipos):
+    mostrar_equipos(equipos)
+    num_equipo = int(input("Seleccione el número del equipo para mostrar jugadores: ")) - 1
+    
+    if 0 <= num_equipo < len(equipos):
+        equipo_seleccionado = equipos[num_equipo]
+        print(f"Jugadores del equipo '{equipo_seleccionado.nombre}':")
+        for jugador in equipo_seleccionado.jugadores:
+            print(f"Nombre: {jugador.nombre}, Número: {jugador.numero}, Nacionalidad: {jugador.nacionalidad}, Posición: {jugador.posicion}, Rol: {jugador.rol}")
+    else:
+        print("Número de equipo inválido.")
 
-print("-------------------")
-#Resultados de los partidos
-print("Resultados de los partidos")
-print("-------------------")
+def mostrar_partidos(equipos, torneo):
+    print("Partidos del torneo:")
+    for partido in torneo.partidos:
+        resultado_encontrado = False
+        for resultado in torneo.resultados:
+            if resultado.partido == partido:
+                print(f"{partido.equipo1.nombre} vs {partido.equipo2.nombre} en el estadio {partido.estadio} el {partido.fecha}")
+                resultado_encontrado = True
+                break
+        if not resultado_encontrado:
+            print(f"{partido.equipo1.nombre} vs {partido.equipo2.nombre} en el estadio {partido.estadio} el {partido.fecha} (sin resultado)")
 
-resultado1 = Resultado((partido1), 2, 1)
-resultado2 = Resultado((partido2), 0, 1)
-resultado3 = Resultado((partido3), 3, 2)
 
-for resultado in [resultado1, resultado2, resultado3]:
-    print(f"Partido: {resultado.partido.equipo1.nombre} vs {resultado.partido.equipo2.nombre}")
-    print(f"Estadio: {resultado.partido.estadio}, Fecha: {resultado.partido.fecha}")
-    print(f"Goles: {resultado.goles_e1} - {resultado.goles_e2}")
-    print("------------------------")
+def main():
+    equipos = []
+    torneo = crear_torneo()
 
+    while True:
+        mostrar_menu()
+        opcion = input("Seleccione una opción: ")
+
+        if opcion == "1":
+            equipo = agregar_equipo()
+            equipos.append(equipo)
+            print("Equipo agregado con éxito.")
+        elif opcion == "2":
+            if equipos:
+                agregar_jugadores(equipos)
+            else:
+                print("No hay equipos disponibles. Agregue equipos primero.")
+        elif opcion == "3":
+            if equipos:
+                agregar_equipos_al_torneo(torneo, equipos)
+            else:
+                print("No hay equipos disponibles. Agregue equipos primero.")
+        elif opcion == "4":
+            num_partidos = int(input("Ingrese la cantidad de partidos que desea agregar: "))
+            for i in range(num_partidos):
+                print(f"\nPartido {i+1}:")
+                agregar_partido_al_torneo(torneo)
+        elif opcion == "5":
+            num_resultados = int(input("Ingrese la cantidad de resultados que desea ingresar: "))
+            for i in range(num_resultados):
+                print(f"\nResultado {i+1}:")
+                ingresar_resultados(torneo)
+        elif opcion == "6":
+            mostrar_jugadores(equipos)
+        elif opcion == "7":
+            mostrar_partidos(equipos, torneo)
+        elif opcion == "8":
+            torneo.tabla_posiciones.mostrar_tabla_posiciones()
+        elif opcion == "9":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción inválida. Por favor, seleccione una opción válida.")
+
+# Ejecutar el programa principal
+if __name__ == "__main__":
+    main()
--- models.py
+++ models.py
@@ -1,13 +1,10 @@
 class Equipo:
-   def __init__(self,logotipo, jugadores, dir_tecnico,nombre):
+    def __init__(self, logotipo, jugadores, dt, nombre):
         self.logotipo = logotipo
-        self.jugadores  = jugadores
-        self.dir_tecnico = dir_tecnico
+        self.jugadores = jugadores
+        self.dt = dt
         self.nombre = nombre
-
-   def agregar_jugador(self,jugador):
-    self.jugadores.append(jugador)
-
+       
 class Jugador:
    def __init__(self,nombre, numero, nacionalidad,posicion,rol):
         self.nombre = nombre
@@ -16,22 +13,8 @@ class Jugador:
         self.posicion = posicion
         self.rol = rol
 
-class Torneo:
-   def __init__(self,equipo, partidos,fechas):
-        self.equipo = equipo
-        self.partidos = partidos
-        self.fechas = fechas
-
-   def agregar_partido(self, equipo1, equipo2, estadio, fecha):
-        partido = Partido(equipo1, equipo2, estadio, fecha)
-        self.partidos.append(partido)
-
-   def agregar_equipo(self, equipo):
-        self.equipos.append(equipo)
-
-   def mostrar_partidos(self):
-        for partido in self.torneo.partidos:
-            print(partido.equipo1.nombre, "vs", partido.equipo2.nombre, "en", partido.estadio, "el", partido.fecha)
+   def agregar_jugador(self,jugador):
+    self.jugadores.append(jugador)
 
 
 class DirectorTecnico:
@@ -41,45 +24,120 @@ class DirectorTecnico:
         self.porcentaje = porcentaje
     
     def agregar_director_tecnico(self, director_tecnico):
-        self.agregar_director_tecnico.append(director_tecnico)
+        self.director_tecnico = director_tecnico
+
+class Torneo:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.equipos = []
+        self.partidos = []
+        self.tabla_posiciones = TablaPosiciones(self)  
+        self.resultados = []
+
+    def agregar_equipo(self, equipo):
+        self.equipos.append(equipo)
+        print(f"Equipo '{equipo.nombre}' agregado al torneo '{self.nombre}'")
+
+    def obtener_equipo_torneo(self, equipo):
+        for equipo_torneo in self.tabla_posiciones.equipos_torneo:
+            if equipo_torneo.equipo == equipo:
+                return equipo_torneo
+        # Si no se encuentra el equipo, crear un nuevo objeto EquipoTorneo y agregarlo a la lista
+        equipo_torneo = EquipoTorneo(equipo)
+        self.tabla_posiciones.equipos_torneo.append(equipo_torneo)
+        return equipo_torneo
+
+    def registrar_resultado(self, resultado):
+        partido = resultado.partido
+        equipo1 = partido.equipo1
+        equipo2 = partido.equipo2
+        goles_e1 = resultado.goles_e1
+        goles_e2 = resultado.goles_e2
+
+        partido.mostrar_partido()
+        print(f"Resultado: {equipo1.nombre} {goles_e1} - {goles_e2} {equipo2.nombre}")
+
+        equipo_torneo1 = self.obtener_equipo_torneo(equipo1)  
+        equipo_torneo2 = self.obtener_equipo_torneo(equipo2)  
+
+        if goles_e1 > goles_e2:
+            equipo_torneo1.agregar_puntos(3)
+            equipo_torneo1.agregar_partido_ganado()
+            equipo_torneo2.agregar_partido_perdido()
+            equipo_torneo1.agregar_goles(goles_e1)
+            equipo_torneo2.agregar_goles(goles_e2)
+        elif goles_e1 < goles_e2:
+            equipo_torneo2.agregar_puntos(3)
+            equipo_torneo2.agregar_partido_ganado()
+            equipo_torneo1.agregar_partido_perdido()
+            equipo_torneo1.agregar_goles(goles_e1)
+            equipo_torneo2.agregar_goles(goles_e2)
+        else:
+            equipo_torneo1.agregar_puntos(1)
+            equipo_torneo2.agregar_puntos(1)
+            equipo_torneo1.agregar_partido_empatado()
+            equipo_torneo2.agregar_partido_empatado()
+            equipo_torneo1.agregar_goles(goles_e1)
+            equipo_torneo2.agregar_goles(goles_e2)
+
+        self.resultados.append(resultado)
+
 
 class TablaPosiciones:
-   def __init__(self,torneo, equipos_torneo):
+    def __init__(self, torneo):
         self.torneo = torneo
-        self.equipos_torneo = equipos_torneo
+        self.equipos_torneo = []
 
+    def obtener_equipo_torneo(self, equipo):
+        for equipo_torneo in self.equipos_torneo:
+            if equipo_torneo.equipo == equipo:
+                return equipo_torneo
+        equipo_torneo = EquipoTorneo(equipo)
+        self.equipos_torneo.append(equipo_torneo)
+        return equipo_torneo
 
-   def agregar_goles(self, goles):
-        self.goles += goles
+    def agregar_equipo(self, equipo):
+        equipo_torneo = EquipoTorneo(equipo)
+        self.equipos_torneo.append(equipo_torneo)
 
-   def agregar_puntos(self, puntos):
-        self.puntos += puntos
+    
+    def mostrar_tabla_posiciones(self):
+        # Ordenar los equipos por puntos de mayor a menor
+        self.equipos_torneo.sort(key=lambda x: x.puntos, reverse=True)
 
-   def agregar_partido_ganado(self):
-        self.partidos_ganados += 1
+        print("Tabla de Posiciones:")
+        print("Equipo\t\tPuntos\tPartidos Ganados\tPartidos Empatados\tPartidos Perdidos\tGoles")
 
-   def agregar_partido_empatado(self):
-        self.partidos_empatados += 1
+        for equipo_torneo in self.equipos_torneo:
+            print(f"{equipo_torneo.equipo.nombre}\t{equipo_torneo.puntos}\t{equipo_torneo.partidos_ganados}\t{equipo_torneo.partidos_empatados}\t{equipo_torneo.partidos_perdidos}\t{equipo_torneo.goles}")
 
-   def agregar_partido_perdido(self):
-        self.partidos_perdidos += 1
-   
-            
 
 class EquipoTorneo:
-   def __init__(self,equipo,goles, puntos, partidos_ganados,partidos_empatados,partidos_perdidos):
+    def __init__(self, equipo):
         self.equipo = equipo
-        self.goles = goles
-        self.puntos = puntos
-        self.partidos_ganados = partidos_ganados
-        self.partidos_empatados = partidos_empatados
-        self.partidos_perdidos = partidos_perdidos
-  
-  
-   def mostrar_equipos(self):
-        for equipo in self.equipos:
-            print(equipo.nombre)
-    
+        self.nombre = equipo.nombre 
+        self.puntos = 0
+        self.partidos_ganados = 0
+        self.partidos_empatados = 0
+        self.partidos_perdidos = 0
+        self.goles = 0
+
+
+    def agregar_puntos(self, puntos):
+        self.puntos += puntos
+
+    def agregar_partido_ganado(self):
+        self.partidos_ganados += 1
+
+    def agregar_partido_empatado(self):
+        self.partidos_empatados += 1
+
+    def agregar_partido_perdido(self):
+        self.partidos_perdidos += 1
+
+    def agregar_goles(self, goles):
+        self.goles += goles
+
 
 class Partido:
     def __init__(self, equipo1, equipo2, estadio, fecha):
@@ -87,19 +145,17 @@ class Partido:
         self.equipo2 = equipo2
         self.estadio = estadio
         self.fecha = fecha
-    
+
     def mostrar_partido(self):
         print(f"{self.equipo1.nombre} vs {self.equipo2.nombre} en el estadio {self.estadio} el {self.fecha}")
 
-   
- 
+
 class Resultado:
-   def __init__(self,partido, goles_e1,goles_e2):
+    def __init__(self, partido, goles_e1, goles_e2):
         self.partido = partido
         self.goles_e1 = goles_e1
         self.goles_e2 = goles_e2
 
-   def mostrar_resultado(self):
-        return f"Resultado: {self.partido.mostrar_partido()} - {self.goles_e1}-{self.goles_e2}"
-    
+    def mostrar_resultado(self):
+        print(f"Resultado: {self.partido.equipo1.nombre} {self.goles_e1} - {self.goles_e2} {self.partido.equipo2.nombre}")
 
